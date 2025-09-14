c05e29c293d15419f4dd5a8bd7994aa38ac0e0a4
theCodeKnightRises <223536528+theCodeKnightRises@users.noreply.github.com>
2025-09-14 23:03:21 +0530

    Modify the code so it returns true and false as per the spec file
    - true when number is greater than or equal to 10
    - false for 9 and 6

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,9 +1,11 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
-  }
+	if (number >= 10) {
+		return true;
+	} else if (number == 9 || number == 6) {
+		return false;
+	} else {
+		return false;
+	}
 }
 
 // Do not edit below this line
