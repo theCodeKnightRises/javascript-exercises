7c345c95f6256832044986d19f905882cd00ef42
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2025-10-26 10:48:14 +0530

    Repeat the string using loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    let repeatedString;
+
+    if (!repeatedString && num < 0) {
+        repeatedString = "ERROR"
+    } else if (!repeatedString && !num) {
+        repeatedString = "";
+    } else if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            if (!repeatedString) {
+                repeatedString = str;
+            } else {
+                repeatedString += str;
+            }
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
