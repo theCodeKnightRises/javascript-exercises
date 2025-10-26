c1db5779b4f06d06d511da64d40432c6567a6fd5
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2025-10-26 13:32:55 +0530

    Remove other elements from the array
    - Keep my solution for future reference

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    return arr.filter(item => !theArgs.includes(item));
+
+
+    // my solution
+    // for (const arg of theArgs) {
+    //
+    //     for (const item of arr) {
+    //
+    //         let i = arr.indexOf(arg);
+    //         if (arr.includes(arg)) {
+    //             arr.splice(i, 1);
+    //         }
+    //
+    //     }
+    //
+    // }
+    // return arr;
 };
 
 // Do not edit below this line
