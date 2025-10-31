b520e2808a9c8f865a306f7bde85409cd1f76882
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2025-10-31 23:09:56 +0530

    Return the sum of all integers of arguments

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+
+    if ((num1 >= 0 && num2 >= 0) &&
+        (Number.isInteger(num1) && Number.isInteger(num2))) {
+
+        if (num2 > num1) {
+            for (let i = num1; i <= num2; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = num2; i <= num1; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+
+    }
+
+    return "ERROR";
 };
 
 // Do not edit below this line
