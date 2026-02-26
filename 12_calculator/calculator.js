7b9dacdfa33f6bede23ce0c5cd0596dd1d63a34d
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2026-02-26 23:50:13 +0530

    Finish all calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function (n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+	if (n === 0 || n === 1) return 1;
+	let f = 1;
+	for (i = 1; i <= n; i++) {
+		f = f * i;
+	}
+	return f;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
