91e05f7f902988f31d24e70d05716f19cb999828
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2026-02-27 23:43:48 +0530

    Finish palindromes exercise
    - Replace all the punctuations and space, split, reverse, join and
      converted them to lowercase for comparison

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	return (
+		word
+			.replace(/[^a-zA-Z0-9]/g, '')
+			.split('')
+			.reverse()
+			.join('')
+			.toLowerCase() == word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+	);
 };
 
 // Do not edit below this line
