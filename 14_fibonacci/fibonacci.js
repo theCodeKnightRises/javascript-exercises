094395052d1ba14fa91c46bdb09219af2ad69e38
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2026-03-04 20:33:30 +0530

    Finished fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	n = Number(n);
+	if (n < 0) return 'OOPS';
+	if (n <= 1) return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 // Do not edit below this line
